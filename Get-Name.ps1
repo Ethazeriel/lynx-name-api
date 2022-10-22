40b5e8c34c7dec933b1c41ff0be64068d6ed8b9a
Neil Merchant <neil@lynxedge.com>
2022-10-22 12:35:25 -0700

    fixed client caller

--- Get-Name.ps1
+++ Get-Name.ps1
@@ -1,4 +1,4 @@
-$info = Get-ComputerInfo;
+$info = Get-ComputerInfo | ConvertTo-Json -Depth 5;
 $uri = "http://172.16.12.103:17346/name"
 $name = Invoke-RestMethod -Method Post -Uri $uri -Body $info -Headers @{'content-type' = 'application/json'};
-Write-Output $name
+Write-Output $name;
